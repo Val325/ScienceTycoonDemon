69707d5460dec94d50be8b5f43af2a86953a043f
Val325 <valera777pushkin777@gmail.com>
2023-09-20 21:41:23 +0300

    add saving struct

---
+++ SaveData.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<fstream>
+
+struct Saving {
+    bool Tech20Century;
+    bool Transistor;
+    bool Computer;
+    bool material;
+    bool Mathematics;
+    bool Being;
+    bool NaNtech;
+    bool QuatumTech;
+    bool ColdNuke;
+    bool WarpEngine;
+    bool NeuralNetwork;
+    bool ArtificialIntellect;
+    bool Robot;
+    bool Singularit;
+    Saving(){
+        bool Tech20Century = false;
+        bool Transistor = false;
+        bool Computer = false;
+        bool material = false;
+        bool Mathematics = false;
+        bool Being = false;
+        bool NaNtech = false;
+        bool QuatumTech = false;
+        bool ColdNuke = false;
+        bool WarpEngine = false;
+        bool NeuralNetwork = false;
+        bool ArtificialIntellect = false;
+        bool Robot = false;
+        bool Singularit = false;
+    }
+};
